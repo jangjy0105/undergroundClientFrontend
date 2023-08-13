083de3af6222a66bb01028d93ba869383da84654
jaeyoonC <jjus88@naver.com>
2023-08-14 00:06:13 +0900

    20230730

--- src/tagDetail.js
+++ src/tagDetail.js
@@ -3,15 +3,16 @@ import tagData from "./tagData";
 import { useParams } from "react-router-dom";
 
 function TagDetail({TagId}) {
-    // const {id} = useParams();
+    const {id} = useParams();
 
-    // const TagTitle = tagData.map(tag => tag.id);
-    // console.log(TagTitle)
+    const TagTitle = tagData.map(tag => tag.id);
+    
     
     return(
         <div className="tagmain">
-            <h1 className="topId">
-                재영아 사랑해
+            <h1 className="tagId">
+                {TagTitle[id]}
+                언더그라운드
             </h1>
         </div>
     )
